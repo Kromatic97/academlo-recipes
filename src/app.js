88bf2179deb8b5449bbb3cbf258f347d99414483
Kromatic97 <rogiromero@gmail.com>
2022-10-31 22:56:11 -0300

    add recipes controller

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 //? Dependencies
 const express = require('express');
 const db = require('./utils/database')
+const cors = require('cors')
 
 //? Files
 const {port} = require('./config');
@@ -15,6 +16,7 @@ const initModels = require('./models/initModels')
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 db.authenticate()
     .then(() => {
